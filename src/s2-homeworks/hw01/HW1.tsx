aded4b68aeb93f53a1477f61db6295035cb99c6f
anastasia <legkova.anastasia127@gmail.com>
2023-06-28 09:35:24 +0300

    1hw

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,14 +14,22 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id:number,
+    user: {
+        avatar: string
+        name: string},
+    message: {
+        text:string
+        time:string }
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Valya',  // можно менять
     },
     message: {
         text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
@@ -31,8 +39,8 @@ export const message0: MessageType = {
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
-        avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        avatar: avatar  , // можно менять
+        name: 'Anya', // можно менять
     },
     message: {
         text: 'зеркальное сообщение для тренировки css', // можно менять
