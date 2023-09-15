a0c13df3dd080993d905f7ee61435cf3bcc8417a
anastasia <legkova.anastasia127@gmail.com>
2023-09-15 04:52:25 +0300

    hw-08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,25 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === 'up') {
+                return [...state].sort((a, b) => {
+                   return  a.name.localeCompare(b.name)
+                })
+            } else if (action.payload==='down') {
+                return [...state].sort((a, b) => {
+                    return b.name.localeCompare(a.name)
+                })
+            }
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+           return  action.payload===18?state.filter(u=>u.age>=18): state
         }
         default:
             return state
     }
 }
+
